02339479d7b5a606731c6c3b6bb2dc40b5807c25
Lauren Ugerio <lauren@bidmylisting.com>
2022-06-21 20:06:28 -0700

    Added util folder

---
+++ util/validations.js
@@ -0,0 +1,15 @@
+
+// Validates the pre-login header
+const validatePreLoginHeader = function() {
+  // TODO
+}
+
+// Validates the pre-login footer
+const validatePreLoginFooter = function() {
+  // TODO
+}
+
+module.exports = {
+  validatePreLoginFooter,
+  validatePreLoginHeader
+};
