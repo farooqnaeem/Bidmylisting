0d82a2ddf6763677cd9e2376278be45c90612038
Lauren Ugerio <lauren@bidmylisting.com>
2022-08-02 10:18:37 -0700

    Fixed typo and added checks for disabled btn

--- util/common.js
+++ util/common.js
@@ -16,21 +16,34 @@ async function testHomeownerRegistrationAddressLookup (page) {
   await page.keyboard.press('ArrowDown');
   await page.waitForTimeout(1000);
   await page.keyboard.press('Enter');
+
+  // TODO add tests for zip code input
 }
 
 async function testAgentCreateAccount(page) {
   await expect(page).toHaveURL('/registration/agent/create-account');
 
+  const submitBtn = await page.locator(testIds.agentRegistration.createBtn);
+  await expect(submitBtn).toBeDisabled();
+
   // Enter agent details
   await page.locator(testIds.agentRegistration.firstName).fill(testData.agentRegistration.firstName);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.lastName).fill(testData.agentRegistration.lastName);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.email).fill(testData.agentRegistration.email);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.emailConf).fill(testData.agentRegistration.email);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.phoneNumber).fill(testData.agentRegistration.phone);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.zipcode).fill(testData.agentRegistration.zipcode);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.password).fill(testData.agentRegistration.password);
+  await expect(submitBtn).toBeDisabled();
   await page.locator(testIds.agentRegistration.termCheckbox).click();
   await page.waitForTimeout(1000);
+  await expect(submitBtn).not.toBeDisabled();
 }
 
 module.exports = {
