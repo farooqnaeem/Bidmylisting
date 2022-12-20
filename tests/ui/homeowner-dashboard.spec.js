c428c82241cc8f174b080d3f53ed58e251b4b991
farooqnaeem <itsfarooqnaeem@gmail.com>
2022-12-21 03:45:55 +0500

    text updated

--- tests/ui/homeowner-dashboard.spec.js
+++ tests/ui/homeowner-dashboard.spec.js
@@ -2,7 +2,7 @@ const { test } = require('@playwright/test');
 const dashboard = require('../../testPages/homeowner/dashboard');
 test.describe('Login Tests', () => {
 
-  test('The user automate the complete scenario of create listings', async ({page}) => {
+  test('Dashboard Listing Info ', async ({page}) => {
     await dashboard.dashboardListingInfo(page);
   });
 });
