3aaceee7ef368e46a39d4501515c7a43815679f5
Lauren Ugerio <lauren@bidmylisting.com>
2022-12-20 11:34:15 -0800

    Removing local config stuff

--- playwright.config.js
+++ playwright.config.js
@@ -4,12 +4,8 @@ require('dotenv').config();
 
 if (process.env.BML_ENV == 'CI') {
   module.exports = require('./playwright.ci.config');
-} else if (process.env.BML_ENV == 'QA') {
-  module.exports = require('./playwright.qa.config');
 } else if (process.env.BML_ENV == 'PROD') {
   module.exports = require('./playwright.prod.config');
 } else { 
-  const config = require('./playwright.qa.config');
-  const local = require('./playwright.local.config');
-  module.exports = Object.assign({}, config, local);
+  module.exports = require('./playwright.qa.config');
 }
